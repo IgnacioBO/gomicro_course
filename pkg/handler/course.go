6fbd222a428b299ae0435f15407bd91f0c17fa9c
IgnacioBO <ignacio.barria.o@gmail.com>
2025-03-05 19:45:49 -0300

    change mux to gin gonic

--- pkg/handler/course.go
+++ pkg/handler/course.go
@@ -13,14 +13,15 @@ import (
 
 	"github.com/IgnacioBO/go_lib_response/response"
 	"github.com/IgnacioBO/gomicro_course/internal/course"
+	"github.com/gin-gonic/gin"
 	"github.com/go-kit/kit/endpoint"
 	httptransport "github.com/go-kit/kit/transport/http"
-	"github.com/gorilla/mux"
 )
 
 // Este recibe contexto y un endpoint que definimos en la capa del endpionts
 func NewUserHTTPServer(ctx context.Context, endpoints course.Endpoints) http.Handler {
-	router := mux.NewRouter()
+	//GIN (con mux era router := mux.NewRouter())
+	router := gin.Default()
 
 	//Esta se guarad en opciones y se pone al final en Handle
 	opciones := []httptransport.ServerOption{
@@ -28,42 +29,70 @@ func NewUserHTTPServer(ctx context.Context, endpoints course.Endpoints) http.Han
 	}
 
 	//Ahora usaremos Handle, poreque a este se le puede pasar un server (httptranpsort)
+	//Ahora usaremos GIN en vez de mux
+	//router.POST recibe un string y una funcion [HandlerFunc] (antes aceptaba una interface http.Handler)
+	//Entonces tenemos un WrhapH que toma este http.Handler y devuelve un HandlerFunc para que quede ok y router.POST no de error
+	router.POST("/courses", gin.WrapH(httptransport.NewServer(
+		endpoint.Endpoint(endpoints.Create),
+		decodeCreateCourse,
+		encodeResponse,
+		opciones...,
+	)))
+
+	/* Post antiguo con Mux
 	router.Handle("/courses", httptransport.NewServer(
 		endpoint.Endpoint(endpoints.Create),
 		decodeCreateCourse,
 		encodeResponse,
 		opciones...,
-	)).Methods("POST")
+	)).Methods("POST")*/
 
+	router.GET("/courses", gin.WrapH(httptransport.NewServer(
+		endpoint.Endpoint(endpoints.GetAll),
+		decodeGetAllCourse,
+		encodeResponse,
+		opciones...,
+	)))
+
+	//Aparte del gin.WrapH; le enviaremos ANTES un ginDecode (que es un metodo que creamos que hace que el request tenga un contexto con "params" que son los parametros)
+	router.GET("/courses/:id", ginDecode, gin.WrapH(httptransport.NewServer(
+		endpoint.Endpoint(endpoints.Get),
+		decodeGetCourse,
+		encodeResponse,
+		opciones...,
+	)))
+
+	/* Get antiguo con Mux
 	router.Handle("/courses/{id}", httptransport.NewServer(
 		endpoint.Endpoint(endpoints.Get),
 		decodeGetCourse,
 		encodeResponse,
 		opciones...,
-	)).Methods("GET")
+	)).Methods("GET")*/
 
-	router.Handle("/courses", httptransport.NewServer(
-		endpoint.Endpoint(endpoints.GetAll),
-		decodeGetAllCourse,
-		encodeResponse,
-		opciones...,
-	)).Methods("GET")
-
-	router.Handle("/courses/{id}", httptransport.NewServer(
+	router.DELETE("/courses/:id", ginDecode, gin.WrapH(httptransport.NewServer(
 		endpoint.Endpoint(endpoints.Delete),
 		decodeDeleteCourse,
 		encodeResponse,
 		opciones...,
-	)).Methods("DELETE")
+	)))
 
-	router.Handle("/courses/{id}", httptransport.NewServer(
+	router.PATCH("/courses/:id", ginDecode, gin.WrapH(httptransport.NewServer(
 		endpoint.Endpoint(endpoints.Update),
 		decodeUpdateCourse,
 		encodeResponse,
 		opciones...,
-	)).Methods("PATCH")
+	)))
 
 	return router
+}
+
+// MIDDLEWARE DE GIN, QUE RCIEB UN CONTEXTO DE GIN, OSEA RECIBE UN CONTEXTO DE GIN gin.Context y TOMA EL CONTEXTO de GIN
+// Entonces le pasaremos por contexto, definiermos un valor en el contexto q se llamara "params" que tendra los aprametros
+// Entonces ahora el contexto de tipo context.Context [que es el que usa nuestros decodes] (NO el gin.Context) tendra el contexto de gin.Context!
+func ginDecode(c *gin.Context) {
+	ctx := context.WithValue(c.Request.Context(), "params", c.Params) //Le decimos que que el ctx tendra una llave "params" que devueve un c.Params [que sirve para obtener parametros]
+	c.Request = c.Request.WithContext(ctx)                            //Genero el request con el contexto, para que el requeste VALLA CON ESE CONTEXT y pueda obtenerse en los decode
 }
 
 // *** MIDDLEWARE REQUEST ***
@@ -106,15 +135,24 @@ func encodeError(_ context.Context, err error, w http.ResponseWriter) {
 }
 
 // *** MIDDLEWARE REQUEST GET ***
-func decodeGetCourse(_ context.Context, r *http.Request) (interface{}, error) {
+// Ahora SI RECIBIERMO EL CONTEXTO, con ctx (antes _)
+func decodeGetCourse(ctx context.Context, r *http.Request) (interface{}, error) {
 	//Le pasamos un validador de ahtotization (que le pasamos del token del header)
 	if err := authorization(r.Header.Get("Authorization")); err != nil {
 		return nil, response.Forbidden(err.Error())
 	}
 
 	var getReq course.GetRequest
+	/* Antiguio metodo con mux
 	variablesPath := mux.Vars(r)
 	getReq.ID = variablesPath["id"] //OBtenemos el id y lo guardamos en el cmapo ID de getReq
+	*/
+
+	//Obtendremos params, que se lo dimos con la funcion ginDecode
+	//Value devuelve un any/interface [osea cualquier cosa], tenemos que convertir al objeto que quiero recibir (el parametro de gin, gin.Params)
+	params := ctx.Value("params").(gin.Params) // .(gin.Params) es para transfmar o asegur que el objeto sera un gun.Params
+
+	getReq.ID = params.ByName("id") //Con byName obtenemos el valor de id
 
 	fmt.Println("id es:", getReq.ID)
 
@@ -147,14 +185,15 @@ func decodeGetAllCourse(_ context.Context, r *http.Request) (interface{}, error)
 }
 
 // *** MIDDLEWARE REQUEST Delete ***
-func decodeDeleteCourse(_ context.Context, r *http.Request) (interface{}, error) {
+func decodeDeleteCourse(ctx context.Context, r *http.Request) (interface{}, error) {
 	//Le pasamos un validador de ahtotization (que le pasamos del token del header)
 	if err := authorization(r.Header.Get("Authorization")); err != nil {
 		return nil, response.Forbidden(err.Error())
 	}
 
-	variablesPath := mux.Vars(r)
-	id := variablesPath["id"]
+	params := ctx.Value("params").(gin.Params) // .(gin.Params) es para transfmar o asegur que el objeto sera un gun.Params
+	id := params.ByName("id")                  //Con byName obtenemos el valor de id
+
 	fmt.Println("id a eliminar es:", id)
 	deleteReq := course.DeleteRequest{ID: id}
 
@@ -163,7 +202,7 @@ func decodeDeleteCourse(_ context.Context, r *http.Request) (interface{}, error)
 }
 
 // *** MIDDLEWARE REQUEST Delete***
-func decodeUpdateCourse(_ context.Context, r *http.Request) (interface{}, error) {
+func decodeUpdateCourse(ctx context.Context, r *http.Request) (interface{}, error) {
 	//Le pasamos un validador de ahtotization (que le pasamos del token del header)
 	if err := authorization(r.Header.Get("Authorization")); err != nil {
 		return nil, response.Forbidden(err.Error())
@@ -175,9 +214,10 @@ func decodeUpdateCourse(_ context.Context, r *http.Request) (interface{}, error)
 	if err != nil {
 		return nil, response.BadRequest(fmt.Sprintf("invalid request format: '%v'", err.Error()))
 	}
+	print("Lol")
 
-	variablesPath := mux.Vars(r)
-	reqStruct.ID = variablesPath["id"]
+	params := ctx.Value("params").(gin.Params) // .(gin.Params) es para transfmar o asegur que el objeto sera un gun.Params
+	reqStruct.ID = params.ByName("id")         //Con byName obtenemos el valor de id
 
 	return reqStruct, nil
 
